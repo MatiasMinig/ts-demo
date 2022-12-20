051174492af68b7735617fef8ca7600d731141ef
Matias Minig <matiasminig@gmail.com>
2022-12-20 18:37:39 -0300

    Segundo Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ts-demo
 Proyecto de ejemplo para desarrollo con TS
+20/12/2022 Siguiendo Practicas.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hola typescript");
+//console.log("hola Matias");
+//console.log("Adios Matias");
+
+// Declarar Variables:
+var nombre = "Matias";
+
+
+console.log('Hola, ' + nombre);
+console.log(`¿Cómo han ido las vacaciones?, ${nombre}`);
+
+let email = "matias@imaginagroup.com"; // Variable de ámbito local
+console.log(`Email de ${nombre} es ${email}`);
+
+// 
